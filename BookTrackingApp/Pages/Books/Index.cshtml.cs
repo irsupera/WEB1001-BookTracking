be93889b824fdf48c8c5a2dfd13b88bce2baf46c
irsupera <97858090+irsupera@users.noreply.github.com>
2022-04-22 03:55:01 -0400

    Add schema and styles
    Add schema and styles

--- BookTrackingApp/Pages/Books/Index.cshtml.cs
+++ BookTrackingApp/Pages/Books/Index.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using BookTrackingApp.Models;
+using Schema.NET;
 
 namespace BookTrackingApp.Pages.Books
 {
@@ -13,17 +14,27 @@ namespace BookTrackingApp.Pages.Books
     {
         private readonly Context_Book _context;
 
+        public Thing JSONLD { get; set; } = new Thing();
+
         public IndexModel(Context_Book context)
         {
             _context = context;
         }
 
-        public IList<Book> Book { get;set; }
+        public IList<Models.Book> Book { get; set; }
 
         public async Task OnGetAsync()
         {
             Book = await _context.Book
                 .Include(b => b.Category).ToListAsync();
+
+            Schema.NET.Book book = new Schema.NET.Book();
+
+            book.About = new OneOrMany<IThing>(new List<Thing>() { new Thing() { Name = "Test Book" } });
+            book.Name = @"Test Book";
+
+            JSONLD = book;
         }
+
     }
 }
